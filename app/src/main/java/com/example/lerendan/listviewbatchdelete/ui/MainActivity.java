b1e7dec42146d54a7ef612e2f93897e0e3b3bde2
Lerendan <340212100@qq.com>
2016-08-01 16:28:32 +0800

    add dialog

--- app/src/main/java/com/example/lerendan/listviewbatchdelete/ui/MainActivity.java
+++ app/src/main/java/com/example/lerendan/listviewbatchdelete/ui/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.lerendan.listviewbatchdelete.ui;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -40,9 +43,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-        mListview = (ListView)findViewById(R.id.lv);
-        btn_confirm = (Button)findViewById(R.id.confirm);
+        mListview = (ListView) findViewById(R.id.lv);
+        btn_confirm = (Button) findViewById(R.id.confirm);
     }
+
     private void initData() {
         if (mAdapter == null) {
             mData = getCheckData();
@@ -54,15 +58,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
     private List<Check> getCheckData() {
         List<Check> data = new ArrayList<Check>();
         Check check1 = new Check("id111111111111", "张三 11111111");
         Check check2 = new Check("id222222222222", "但超 22222222");
-        Check check3 = new Check("id333333333333","李四 33333333");
-        Check check4 = new Check("id444444444444","王五 44444444");
+        Check check3 = new Check("id333333333333", "李四 33333333");
+        Check check4 = new Check("id444444444444", "王五 44444444");
         Check check5 = new Check("id555555555555", "赵六 55555555");
         Check check6 = new Check("id666666666666", "张七 66666666");
-        Check check7 = new Check("id777777777777","吴八 77777777");
+        Check check7 = new Check("id777777777777", "吴八 77777777");
         Check check8 = new Check("id888888888888", "关关 88888888");
         Check check9 = new Check("id999999999999", "笑笑 99999999");
         data.add(check1);
@@ -76,6 +81,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         data.add(check9);
         return data;
     }
+
     private void setListener() {
         mListview.setOnItemClickListener(new MyOnItemClickListner());
         btn_confirm.setOnClickListener(this);
@@ -86,7 +92,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.confirm:
                 if (n != 0) {
-                            deleteCheckedItem(); //验证完成后删除选中的item
+                    dialog();//调用确认对话框
                 } else {
                     Toast.makeText(this, "未点击", Toast.LENGTH_SHORT).show();
                 }
@@ -96,6 +102,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+    }
+
+    /**
+     * 确认删除的对话框
+     */
+    public void dialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("确认删除吗？");
+        builder.setTitle("提示");
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                deleteCheckedItem();//确定后删除选中的item
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create();
+        builder.setCancelable(false);//调用这个方法时，按对话框以外的地方不起作用。按返回键也不起作用
+        builder.show();
     }
 
     /**
@@ -116,7 +147,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_confirm.setText("确认（" + n + "）");
     }
 
-    //Listview Item的点击事件
+    /**
+     * Listview Item的点击事件
+     */
     private class MyOnItemClickListner implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
